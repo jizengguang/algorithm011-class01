5118a9b7228208fd2103ce37a78deb80b9d1c4a5
jizengguang <1107873030@qq.com>
2020-07-13 00:04:27 +0800

    add 50

---
+++ Week_03/50pown.go
@@ -0,0 +1,29 @@
+package Week_03
+
+func pown(x float64, n int) float64 {
+	//2、递归
+	//时间复杂度：O(N)
+
+	//n < 0 情况
+	if n < 0 {
+		n *= -1
+		x = 1 / x
+	}
+	return fast(x, n)
+
+}
+
+func fast(x float64, n int) float64 {
+	if n == 0 {
+		return 1.0
+	}
+
+	subResult := fast(x, n/2)
+
+	//判断奇偶决定是否多乘一个X
+	if n&1 == 1 {
+		return subResult * subResult * x
+	}
+	return subResult * subResult
+
+}
