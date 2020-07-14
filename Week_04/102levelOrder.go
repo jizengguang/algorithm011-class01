07b45c91e194fb9dea7a32d32e0fab3265dfd907
jizengguang <1107873030@qq.com>
2020-07-15 00:04:47 +0800

    add 102

--- Week_04/102levelOrder.go
+++ Week_04/102levelOrder.go
@@ -6,13 +6,13 @@ type levelOrderTreeNode struct {
 	Right *levelOrderTreeNode
 }
 
+/*
 var res [][]int
 
 func levelOrder(root *levelOrderTreeNode) [][]int {
 	if root == nil {
 		return nil
 	}
-
 	res = make([][]int, 0)
 	bfs(root, 0)
 	return res
@@ -31,4 +31,39 @@ func bfs(root *levelOrderTreeNode, level int) {
 
 	bfs(root.Left, level+1)
 	bfs(root.Right, level+1)
+}
+*/
+
+//刻意练习dfs
+func levelOrder(root *levelOrderTreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+
+	var res [][]int
+	var queue = make([]*levelOrderTreeNode, 0)
+
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		ans := make([]int, 0)
+		length := len(queue)
+
+		for i := 0; i < length; i++ {
+			node := queue[i]
+			ans = append(ans, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		res = append(res, ans)
+		queue = queue[length:]
+
+	}
+	return res
+
 }
