25e7ed6ca4465ecddff7ef215250a57239cb23d3
jizengguang <1107873030@qq.com>
2020-08-09 23:39:41 +0800

    add 208

--- Week_07/208generateTrie.go
+++ Week_07/208generateTrie.go
@@ -1,5 +1,44 @@
 package Week_07
 
-type Tire struct {
+type Trie struct {
+	IsEnd bool
+	next  [26]*Trie
+}
 
+func Constructor() Trie {
+	return Trie{}
+}
+
+func (trie *Trie) Insert(word string) {
+	for _, v := range word {
+		if trie.next[v-'a'] == nil {
+			trie.next[v-'a'] = new(Trie)
+		}
+		trie = trie.next[v-'a']
+	}
+	trie.IsEnd = true
+}
+
+func (trie *Trie) Search(word string) bool {
+	for _, v := range word {
+		if trie.next[v-'a'] == nil {
+			return false
+		}
+		trie = trie.next[v-'a']
+	}
+	if trie.IsEnd == false {
+		return false
+	}
+
+	return true
+}
+
+func (trie *Trie) StartWith(prefix string) bool {
+	for _, v := range prefix {
+		if trie.next[v-'a'] == nil {
+			return false
+		}
+		trie = trie.next[v-'a']
+	}
+	return true
 }
