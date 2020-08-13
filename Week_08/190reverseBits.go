b2d5d427a715307eee588067eb300546f4c4d764
jizengguang <1107873030@qq.com>
2020-08-14 00:06:17 +0800

    add 190

---
+++ Week_08/190reverseBits.go
@@ -0,0 +1,13 @@
+package Week_08
+
+func reverseBits(num uint32) uint32 {
+	var res = uint32(0)
+	var power = uint32(31)
+
+	for num != 0 {
+		res = res + (num & 1) << power
+		num = num >> 1
+		power -= 1
+	}
+	return res
+}
