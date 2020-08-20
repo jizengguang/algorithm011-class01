c5c09803b07609bb3e0f5628a83e5fd313ede247
jizengguang <1107873030@qq.com>
2020-08-20 22:47:43 +0800

    add mergeSort

--- Week_08/inserSort.go
+++ Week_08/inserSort.go
@@ -5,16 +5,14 @@ import "fmt"
 func insertSort(arr []int) {
 	l := len(arr)
 	for i := 1; i < l; i++ {
-		for j := 0; j < i; j++ {
-			if arr[j] > arr[i] {
-				arr[j], arr[i] = arr[i], arr[j]
-			}
+		for j := 0; j < i && arr[j] > arr[i]; j++ {
+			arr[i], arr[j] = arr[j], arr[i]
 		}
 	}
 }
 
 func main() {
-	var nums = []int{9,8,7,6,5,4,3,2,1}
+	var nums = []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
 	insertSort(nums)
 	fmt.Println(nums)
 }
---
+++ Week_08/mergeSort.go
@@ -0,0 +1,47 @@
+package Week_08
+
+import "fmt"
+
+func mergeSort(arr []int, begin, end int) []int {
+	l := len(arr)
+	if l < 2 {
+		return arr
+	}
+	var medium = begin + end>>1
+	var left = arr[:medium]
+	var right = arr[medium:]
+
+	return merge(mergeSort(left, 0, len(left)-1), mergeSort(right, 0, len(right)-1))
+
+}
+
+func merge(left, right []int) []int {
+	var result []int
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] < right[0] {
+			result = append(result, left[0])
+			left = left[1:]
+		}
+		result = append(result, right[0])
+		right = right[1:]
+
+	}
+
+	for len(left) > 0 {
+		result = append(result, left[0])
+		left = left[1:]
+	}
+
+	for len(right) > 0 {
+		result = append(result, right[0])
+		right = right[1:]
+	}
+
+	return result
+}
+
+func main() {
+	var nums = []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+	fmt.Println(mergeSort(nums, 0, len(nums)-1))
+
+}
