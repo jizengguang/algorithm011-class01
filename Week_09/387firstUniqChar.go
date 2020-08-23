f41ac3c09f603b01ba554049f8adb12ccc95c2dd
jizengguang <1107873030@qq.com>
2020-08-24 00:06:51 +0800

    add 387

---
+++ Week_09/387firstUniqChar.go
@@ -0,0 +1,16 @@
+package Week_09
+
+func firstUniqChar(s string) int {
+	var arr [26]int
+	for i, k := range s {
+		arr[k-'a'] = i
+	}
+	for i, k := range s {
+		if i == arr[k-'a'] {
+			return i
+		} else {
+			arr[k-'a'] = -1
+		}
+	}
+	return -1
+}
